0f01beb2779a7776f9a014f286ca80290357daa4
Nichita Zloteanu <nizl@consignor.com>
2019-07-22 12:06:44 +0300

    Initial Commit

---
+++ CoolApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoolApp
+//
+//  Created by Nichita Zloteanu on 22/07/2019.
+//  Copyright © 2019 Nichita Zloteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
